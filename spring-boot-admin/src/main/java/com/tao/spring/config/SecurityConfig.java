6ee6330ba472148d36cd30cb7353bf0e2c38d20a
tao.dong <tao.dong@ruisdata.com>
2019-04-19 16:52:19 +0800

    Update : use spring boot admin

---
+++ spring-boot-admin/src/main/java/com/tao/spring/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.tao.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author DongTao
+ * @since 2019-04-19
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll()
+                .and()
+                .csrf()
+                .disable();
+    }
+}
