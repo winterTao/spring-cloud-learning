fae583d419f04eabc3b6f804b66b9ded740dd210
tao.dong <tao.dong@ruisdata.com>
2019-05-24 21:47:28 +0800

    Update : 添加config server

---
+++ config-server/src/main/java/com/tao/spring/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.tao.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author DongTao
+ * @since 2019-05-22
+ */
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
