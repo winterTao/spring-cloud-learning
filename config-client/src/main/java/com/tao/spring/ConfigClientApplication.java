f0fdb08e74be5851c0aa86d5ef9b3558c523b25f
tao.dong <tao.dong@ruisdata.com>
2019-05-24 21:47:47 +0800

    Update : 添加config client

---
+++ config-client/src/main/java/com/tao/spring/ConfigClientApplication.java
@@ -0,0 +1,18 @@
+package com.tao.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author DongTao
+ * @since 2019-05-22
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
