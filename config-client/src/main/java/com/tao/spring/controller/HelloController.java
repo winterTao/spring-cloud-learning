37d5042bc8aac99c17ab0f9e61d677b3cfef7dc8
tao.dong <tao.dong@ruisdata.com>
2019-05-27 21:13:25 +0800

    Update : 添加刷新config server配置

--- config-client/src/main/java/com/tao/spring/controller/HelloController.java
+++ config-client/src/main/java/com/tao/spring/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.tao.spring.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author DongTao
  * @since 2019-05-22
  */
+@RefreshScope
 @RestController
 class HelloController {
 
